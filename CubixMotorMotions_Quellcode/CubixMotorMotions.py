b50716e63198eec11dc435669caf8eeb3cb31754
chikizikikato <38123657+chikizikikato@users.noreply.github.com>
2020-07-11 23:13:50 +0200

    Update CubixMotorMotions.py

--- CubixMotorMotions_Quellcode/CubixMotorMotions.py
+++ CubixMotorMotions_Quellcode/CubixMotorMotions.py
@@ -38,42 +38,34 @@ class EdgePathSearcher():
     def search_path_to_edge(self, start, target_edge, max_depth):
         for i in range(1, max_depth+1):
             self.path.clear()
+            self.visited_nodes.clear()
             self._depth_limited_search(start, target_edge, i)
             if(len(self.path)>0): #TODO: Search best path, not first we encounter!
                 return self.path
         raise NoPathException("No Valid Path from {0} to reach edge {1}".format(str(start), str(target_edge)))
 
     def _depth_limited_search(self, start, target_edge, max_depth):
-        print("Searching for depth: {0}".format(str(max_depth)))
-        #print("visited nodes: {0}".format(str(self.visited_nodes)))
-        if(max_depth==0 or start in self.visited_nodes):
+        if(max_depth==0):
             return False
-        #print("adding visited_nodes: {0}".format(str(start)))
-        self.visited_nodes.append(start)
         for neighbor in self.g.neighbors(start):
-           # print("proccessing neighbor: {0}".format(str(neighbor)))
             if(self.g.get_edge_data(start, neighbor).get(self.edge_attribute, None)==target_edge):
                 self.path.insert(0, neighbor)
                 print("Found edge {0} between {1} and {2} !!".format(str(target_edge), str(start), str(neighbor)))
                 return True
             elif(not isinstance(self.g.get_edge_data(start, neighbor).get(self.edge_attribute, None), rotations.NoRotationTurn)):
-                print("another rotation between {0} and {1}".format(str(start), str(neighbor)))
                 continue
             if(self._depth_limited_search(neighbor, target_edge, max_depth-1)):
                 print("Found path somewhere in the chain! adding neighbor: {0}".format(str(neighbor)))
                 self.path.insert(0, neighbor)
                 return True
-            #print("continuing with next neigbor...")
-        #print("removing visited_nodes: {0}".format(str(start)))
-        self.visited_nodes.remove(start)
 
 
 class MoveCubeMap(dict):
     def apply_cube_turn(self, specific_cube_rotation_turn):
-        for k, v in specific_cube_rotation_turn.items():
-            self[self[k]] = v
+        print(str(specific_cube_rotation_turn))
+        self.update({self.get_mapped_face(v): k for k,v in specific_cube_rotation_turn.items()})
 
-    def get_who_has_mapped(self, face_id):
+    def get_mapped_face(self, face_id):
         for mapped_face, face in self.items():
             if(face_id==face):
                 return mapped_face
@@ -101,15 +93,17 @@ class MotorMovesSolver():
 
     def solve(self):
         for move in self.moves_pool.get_pool():
-            if(move.face not in self.current_motor_state):
-                print("No mapping to {0}. Searching for mapping....".format(str(move.face)))
-                cube_turn_comb = self._search_cube_rotation_combination_for_needed_face_id_to_be_mapped(move.face)
+            transformed_face = self.move_cube_map.get_mapped_face(move.face)
+            print("Searching for path to {0}... -> Transformed to {1}".format(str(move.face), str(transformed_face)))
+            if(transformed_face not in self.current_motor_state):
+                print("No mapping to {0}. Searching for mapping....".format(str(transformed_face)))
+                cube_turn_comb = self._search_cube_rotation_combination_for_needed_face_id_to_be_mapped(transformed_face)
                 print("Found cube_turn_combination: {0}. Searching for path...".format(str(cube_turn_comb)))
                 self._search_path_for_cube_rotation_turn_combination(cube_turn_comb)
                 print("Found path!!")
                 self.path.print_path()
-            transformed_face = self.move_cube_map.get_who_has_mapped(move.face)
-            print("Searching for path to {0}... -> Transformed to {1}".format(str(move.face), str(transformed_face)))
+                transformed_face = self.move_cube_map.get_mapped_face(transformed_face)
+            print("Correct Mapping for {0} -> transformed to {1}. Searching path...".format(str(move.face), str(transformed_face)))
             self._search_path_for_move_face(transformed_face)
             print("Found path!!")
             self.path.print_path()
@@ -118,10 +112,8 @@ class MotorMovesSolver():
     def _search_cube_rotation_combination_for_needed_face_id_to_be_mapped(self, face_id):
         for cube_turn_comb in itertools.product(itertools.chain(rotations.CubeRotationTurn.y_axis_cube_turn().cube_rotation_turn_mapping, rotations.CubeRotationTurn.x_axis_cube_turn().cube_rotation_turn_mapping), repeat=2):
             move_cube_map = copy.deepcopy(self.move_cube_map)
-            print("Trying {0}..".format(str(cube_turn_comb)))
             for idx, cube_turn in enumerate(cube_turn_comb):
                 move_cube_map.apply_cube_turn(cube_turn)
-                print(str([move_cube_map[motor_face_id] for motor_face_id in self.current_motor_state]))
                 if(face_id in [move_cube_map[motor_face_id] for motor_face_id in self.current_motor_state]):
                     return cube_turn_comb[:idx+1]
         raise NoPathException("There is no Path to rotate the cube in such a way that {0} is mapped on a motor_arm".format(str(face_id)))
@@ -129,7 +121,6 @@ class MotorMovesSolver():
     def _search_path_for_cube_rotation_turn_combination(self, cube_rotation_turn_combination):
         for cube_turn in cube_rotation_turn_combination:
             cube_rotation_turn = (rotations.CubeRotationTurn.x_axis_cube_turn() if(rotations.CubeRotationTurn.x_axis_cube_turn().is_specific_cube_turn_in_mapping(cube_turn)) else rotations.CubeRotationTurn.y_axis_cube_turn())
-            #print("Searching for {0}..".format(str(cube_rotation_turn)))
             path = EdgePathSearcher(self.g).search_path_to_edge(self.current_motor_state, cube_rotation_turn, self.max_depth)
             self.move_cube_map.apply_cube_turn(cube_turn)
             self._add_path(path)
@@ -155,6 +146,6 @@ if(__name__=="__main__"):
     max_depth = int(sys.argv[2])
 
     moves_pool = moves.MovePool.from_string(moves_string)
-    
+
     solver = MotorMovesSolver(moves_pool, start_motor_map, max_depth=max_depth)
     solver.solve()
